3b8b95dd82998c7a6aa1ccc4d5948bb4f75603dd
Hanabe-Fascart <97356900+Pietro-Porsch-W@users.noreply.github.com>
2025-06-30 22:16:09 -0300

    Create ItemVendaForm.php

---
+++ biblioteca/common/models/ItemVendaForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace common\models;
+
+use yii\base\Model;
+
+class ItemVendaForm extends Model
+{
+    public $exemplar_id;
+    public $quantidade;
+    public $valor_unitario;
+
+    public function rules()
+    {
+        return [
+            [['exemplar_id', 'quantidade', 'valor_unitario'], 'required'],
+            [['exemplar_id'], 'string'],
+            [['quantidade'], 'integer', 'min' => 1],
+            [['valor_unitario'], 'number', 'min' => 0],
+        ];
+    }
+}
